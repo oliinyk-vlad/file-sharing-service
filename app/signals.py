4015303e3056aa5c81a5dedb34e6b7088e22321f
Vladyslav Oliinyk <oliinyk.vladyslav@gmail.com>
2020-07-16 20:27:46 +0300

    added post_delete signal for deleting file

--- app/signals.py
+++ app/signals.py
@@ -1,10 +1,19 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from app.models import File
 from app.tasks import delete_file
+
+import os
 
 
 @receiver(post_save, sender=File)
 def set_deleting_timer(sender, instance, created, **kwargs):
     if created:
         delete_file.apply_async([instance.id], eta=instance.expires_at)
+
+
+@receiver(post_delete, sender=File)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    if instance.source:
+        if os.path.isfile(instance.source.path):
+            os.remove(instance.source.path)
